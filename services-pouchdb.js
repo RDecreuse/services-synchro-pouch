7e28b17eee1ef1fff1cc232436e077a52989613b
RDecreuse <renny.dcrs@gmail.com>
2016-08-12 12:28:15 +0200

    Add services pouch module in angular2 project

---
+++ services-pouchdb.js
@@ -0,0 +1,4 @@
+"use strict";
+
+exports.Animal = require('./dist/model/Animal').Animal;
+exports.AnimalService = require('./dist/service/animal.service').AnimalService;
---
+++ servicesPouch.d.ts
@@ -0,0 +1,2 @@
+export { Animal } from './src/model/Animal';
+export { AnimalService } from './src/service/animal.service';
